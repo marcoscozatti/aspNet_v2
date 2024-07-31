2cb4b2fab402e7fb22ba669a214d0ecd3ebc83f7
Marcos Cozatti <marcoscozatti@gmail.com>
2024-07-31 14:49:46 -0300

    apgando arquivos do projeto

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TI43_teste2.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
